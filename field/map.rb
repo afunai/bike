3ec64162aa7944d0e17ab64906959d83fc9e5ecf
Akira FUNAI <akira@funai.com>
2009-11-01 16:12:56 +0900

    add test_parse_empty_tag()

--- field/map.rb
+++ field/map.rb
@@ -9,16 +9,34 @@ class Map
 
 	private
 
-	def parse_tokens(str)
+	def parse_html(str)
+		meta = {}
+		tmpl = ''
+
+		s = StringScanner.new str
+		until s.eos?
+			if s.scan /(.*?)(\w+):\(/
+				tmpl << s[1]
+				meta[s[2]] = parse_tokens(s)
+			else
+				tmpl << s.scan(/.+/)
+			end
+		end
+		{
+			:meta => meta,
+			:tmpl => tmpl,
+		}
+	end
+
+	def parse_tokens(s)
 		tokens = []
-		s = StringScanner.new(str)
 		until s.eos? || s.scan(/\)/)
 			if s.scan /(["'])(.*?)(\1|$)/
 				tokens << s[2]
 			elsif s.scan /[^\s\)]+/
 				tokens << s[0]
 			else
-				s.scan(/\s+/)
+				s.scan /\s+/
 			end
 		end
 		tokens
--- t/test_parse_html.rb
+++ t/test_parse_html.rb
@@ -17,29 +17,37 @@ class TC_Parse_HTML < Test::Unit::TestCase
 	def test_parse_tokens
 		assert_equal(
 			['foo','bar','baz'],
-			@map.instance_eval { parse_tokens('foo bar baz') },
+			@map.send(:parse_tokens,StringScanner.new('foo bar baz')),
 			'Map#parse_tokens should be able to parse unquoted tokens into array'
 		)
 		assert_equal(
 			['foo','bar','baz baz'],
-			@map.instance_eval { parse_tokens('foo "bar" "baz baz"') },
+			@map.send(:parse_tokens,StringScanner.new('foo "bar" "baz baz"')),
 			'Map#parse_tokens should be able to parse quoted tokens'
 		)
 		assert_equal(
 			['foo','bar','baz'],
-			@map.instance_eval { parse_tokens("foo 'bar' baz") },
+			@map.send(:parse_tokens,StringScanner.new("foo 'bar' baz")),
 			'Map#parse_tokens should be able to parse quoted tokens'
 		)
 
 		assert_equal(
 			['foo','bar','baz'],
-			@map.instance_eval { parse_tokens("foo 'bar' baz) qux") },
+			@map.send(:parse_tokens,StringScanner.new("foo 'bar' baz) qux")),
 			'Map#parse_tokens should stop scanning at an ending bracket'
 		)
 		assert_equal(
 			['foo','bar (bar?)','baz'],
-			@map.instance_eval { parse_tokens("foo 'bar (bar?)' baz) qux") },
+			@map.send(:parse_tokens,StringScanner.new("foo 'bar (bar?)' baz) qux")),
 			'Map#parse_tokens should ignore brackets inside quoted tokens'
+		)
+	end
+
+	def test_parse_empty_tag
+		assert_equal(
+			{'foo' => ['bar','baz baz']},
+			@map.send(:parse_html,'hello foo:(bar "baz baz") world')[:meta],
+			'Map#parse_html should be able to parse empty sofa tags'
 		)
 	end
 

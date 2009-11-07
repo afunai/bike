e1e3bfcae2b8f244e1860dd00fb7feb2c9723644
Akira FUNAI <akira@funai.com>
2009-11-07 17:34:55 +0900

    test_child_folder()

--- field/set_folder.rb
+++ field/set_folder.rb
@@ -8,19 +8,30 @@ class Sofa::Field::Set::Folder < Sofa::Field::Set
 	def initialize(meta = {})
 		meta[:html] = load_html meta
 		super
+		my[:item]['label'] = {:klass => 'text'}
 	end
 
 	private
 
-	def item
-# seek the real directory, then @item_object
+	def collect_item(conditions = :all,&block)
+		if (
+			conditions.is_a?(::String) &&
+			conditions =~ /\A\w+\z/ &&
+			File.directory?(File.join Sofa::ROOT_DIR,my[:dir],conditions)
+		)
+			my[:item][conditions] = {:klass  => 'set-folder'}
+		end
+		super
 	end
 
 	def load_html(meta)
 		meta[:dir] = meta[:parent] ? File.join(meta[:parent][:dir],meta[:id]) : meta[:id]
-		File.open(File.join Sofa::ROOT_DIR,meta[:dir],'_.html') {|f|
-			f.read
-		}
+		html_file = File.join Sofa::ROOT_DIR,meta[:dir],'_.html'
+		if File.exists? html_file
+			File.open(html_file) {|f| f.read }
+		elsif meta[:parent]
+			meta[:parent][:html]
+		end
 	end
 
 end

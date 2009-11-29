9004fef0aa477ec89bc38fcd0d32d47eeb53f98d
Akira FUNAI <akira@funai.com>
2009-11-29 14:18:24 +0900

    Sofa#rebuild_params

--- sofa.rb
+++ sofa.rb
@@ -35,80 +35,41 @@ STORAGE  = {
 		self.session[:client] = id
 	end
 
-	get %r{/(.*/)(.*).(html)} do
-		path,action,ext = *params[:captures]
-		folder = Sofa::Set::Static::Folder.root.item path.split('/')
-		folder.get
-	end
-
+get %r{/(.*/)(.*).(html)} do
+	path,action,ext = *params[:captures]
+	folder = Sofa::Set::Static::Folder.root.item path.split('/')
+	folder.get
 end
 
-__END__
+	private
 
-class Set
-	def initialize(params)
-		params[:html] = load_html(dir) if params[:dir]
-		@meta,@tmpl = parse_html(params[:html])
-# cache meta & tmpl
-		load_items(@meta)
-	end
-	def get(arg = {})
-	end
-end
+	def rebuild_params(src)
+		src.each_key.sort.reverse.inject({}) {|params,key|
+			name,special = key.split('.',2)
+			steps = name.split '-'
 
----
+			if special
+				item_id,special = special.split('-',2)
+			else
+				item_id = steps.pop
+			end
 
-class List
-	attr :items
-	def initialize(dir,parent = nil)
-		@tmpl = ...
-	end
-	def select(list_id,conds = {})
-		selectors = Selector.new(conds)
-		...
-		@items[list_id] = vals.collect {|v| Item.new(dir,self).load(v) }
-	end
-	def result
-		result = {}
-		@items.each {|id,item|
-			result[id] = item.result # item can be scalar or list of item
+			hash = steps.inject(params) {|v,k| v[k] ||= {} }
+			val  = src[key]
+
+			if item_id == 'action'
+				hash[:action] = (special || val).intern
+			elsif item_id == 'conds'
+				hash[:conds] ||= {}
+				hash[:conds][special.intern] = val
+			elsif hash[item_id].is_a? ::Hash
+				hash[item_id][:self] = val
+			else
+				hash[item_id] = val
+			end
+
+			params
 		}
-		@tmpl.gsub(...) {|id| result[id] }
 	end
+
 end
-
-def get
-	chdir {
-		tmpl = parse(tmpl_file)
-		item_files = selector.select()
-		items = item_files.collect {|i|
-			vals = YAML.load(i)
-			tmpl.add_item(vals)
-		}
-	}
-	tmpl.result
-end
-
-<body>
-	<div class="sofa-item" sofa-id="blog">
-		<h2>title:(text 16 0..32)</h2>
-		<p>body*:(wiki 76*10 1..1024)</p>
-		<p>region*:(select "west coast","east coast","southern island","northern area")></p>
-		<div class="sofa-item">
-			image':(img 160*120)
-		</div>
-	</div>
-	<a class="sofa-paging" sofa-id="blog-next">next</a>
-	<a class="sofa-paging" sofa-id="blog-prev">prev</a>
-</body>
-
-static:
-/foo/bar/index.html
-/foo/bar/002.html
-/foo/bar/003.html
-/foo/bar/files/001_of_0001.jpg
-
-dynamic:
-/sofa.rb/foo/bar/index.html
-/sofa.rb/foo/bar/create.html
-/sofa.rb/foo/bar/081026_001/update.html
--- t/test_sofa.rb
+++ t/test_sofa.rb
@@ -41,4 +41,101 @@ class TC_Sofa < Test::Unit::TestCase
 		)
 	end
 
+	def test_rebuild_params
+		sofa = Sofa.new
+
+		hash = sofa.instance_eval {
+			rebuild_params(
+				'.action' => 'update'
+			)
+		}
+		assert_equal(
+			{:action => :update},
+			hash,
+			'Sofa#rebuild_params should be able to rebuild special symbols'
+		)
+
+		hash = sofa.instance_eval {
+			rebuild_params(
+				'.action-update' => 'submit'
+			)
+		}
+		assert_equal(
+			{:action => :update},
+			hash,
+			'Sofa#rebuild_params should be able to rebuild special symbols'
+		)
+
+		hash = sofa.instance_eval {
+			rebuild_params(
+				'noo'               => 'what?',
+				'noo.action-update' => 'submit',
+				'noo.conds-id'      => '4567'
+			)
+		}
+		assert_equal(
+			{
+				'noo' => {
+					:self   => 'what?',
+					:action => :update,
+					:conds  => {:id => '4567'},
+				},
+			},
+			hash,
+			'Sofa#rebuild_params should rebuild both the special symbols and regular items'
+		)
+
+		hash = sofa.instance_eval {
+			rebuild_params(
+				'moo.conds-p'                   => '9',
+				'moo-4567-addr.conds-zip-upper' => '110'
+			)
+		}
+		assert_equal(
+			{
+				'moo' => {
+					:conds => {:p => '9'},
+					'4567' => {
+						'addr' => {
+							:conds => {:'zip-upper' => '110'},
+						},
+					},
+				},
+			},
+			hash,
+			'Sofa#rebuild_params should be able to rebuild any combination of symbols and items'
+		)
+
+		hash = sofa.instance_eval {
+			rebuild_params(
+				'foo-bar.conds-id'  => '1234',
+				'foo-bar.conds-p'   => ['42'],
+				'foo-bar.action'    => 'update',
+				'foo-baz'           => ['boo','bee'],
+				'foo'               => 'oops',
+				'qux.action-create' => 'submit'
+			)
+		}
+		assert_equal(
+			{
+				'foo' => {
+					:self => 'oops',
+					'bar' => {
+						:action => :update,
+						:conds  => {
+							:id => '1234',
+							:p  => ['42'],
+						},
+					},
+					'baz' => ['boo','bee'],
+				},
+				'qux' => {
+					:action => :create,
+				},
+			},
+			hash,
+			'Sofa#rebuild_params should be able to rebuild any combination of symbols and items'
+		)
+	end
+
 end

2a8c017f03e7562c2d72e7971b896fd6700984bc
Akira FUNAI <akira@funai.com>
2009-12-26 00:35:42 +0900

    working...

--- sofa/_storage.rb
+++ sofa/_storage.rb
@@ -34,62 +34,6 @@ class Sofa::Storage
 		item_ids = _page(item_ids,conds)
 	end
 
-def navi(conds)
-	if @sd[:p_size] && !conds[:id]
-		p = conds[:p] || 1
-		p_count = (_item_count(conds) / @sd[:p_size].to_f).ceil
-		sibs = (p_count.to_i > 1) ? (1..p_count).to_a : []
-		navi_prev = conds.merge(:p => p - 1) if p > 1
-		navi_next = conds.merge(:p => p + 1) if p < p_count
-		navi_sibs = {:p => sibs}
-	end
-	if (!navi_prev || !navi_next) && sibs = _sibs(conds)
-		sib_cid  = sibs.keys.first
-		sib_vals = sibs.values.first
-		if i = sib_vals.index(conds[sib_cid])
-			if !navi_prev && i > 0
-				navi_prev = conds.merge(sib_cid => sib_vals[i - 1])
-				navi_prev[conds[:id] ? :id : :p] = :last
-			end
-			if !navi_next && i < (sib_vals.size - 1)
-				navi_next = conds.merge(sib_cid => sib_vals[i + 1])
-				navi_next[conds[:id] ? :id : :p] = 1
-			end
-		end
-	end
-	{
-		:prev => navi_prev,
-		:next => navi_next,
-		:sibs => navi_sibs || {},
-	}
-end
-
-def _item_count(conds)
-	_select_without(:p,conds).size
-end
-
-def _sibs(conds)
-	if cid = (conds.keys - [:p,:order]).find {|k| respond_to?("_sibs_#{k}",true) }
-		sibs = __send__("_sibs_#{cid}",conds)
-		{cid => _sort(sibs,conds)} unless sibs.empty?
-	end
-end
-
-def _sibs_id(conds)
-	_select_without(:id,conds)
-end
-
-def _sibs_d(conds)
-	rex_d = /^\d{#{conds[:d].length}}/
-	_select.collect {|id| id[rex_d] }.uniq
-end
-
-def _select_without(cid,conds)
-	c = conds.dup
-	c.delete cid
-	_select(c)
-end
-
 	def build(v)
 		self
 	end
@@ -104,6 +48,36 @@ end
 
 	def delete(id)
 		id
+	end
+
+	def navi(conds)
+		if @sd[:p_size] && !conds[:id]
+			p = conds[:p] || 1
+			p_count = (_item_count(conds) / @sd[:p_size].to_f).ceil
+			sibs = (p_count.to_i > 1) ? (1..p_count).to_a : []
+			navi_prev = conds.merge(:p => p - 1) if p > 1
+			navi_next = conds.merge(:p => p + 1) if p < p_count
+			navi_sibs = {:p => sibs}
+		end
+		if (!navi_prev || !navi_next) && sibs = _sibs(conds)
+			sib_cid  = sibs.keys.first
+			sib_vals = sibs.values.first
+			if i = sib_vals.index(conds[sib_cid])
+				if !navi_prev && i > 0
+					navi_prev = conds.merge(sib_cid => sib_vals[i - 1])
+					navi_prev[conds[:id] ? :id : :p] = :last
+				end
+				if !navi_next && i < (sib_vals.size - 1)
+					navi_next = conds.merge(sib_cid => sib_vals[i + 1])
+					navi_next[conds[:id] ? :id : :p] = 1
+				end
+			end
+		end
+		{
+			:prev => navi_prev,
+			:next => navi_next,
+			:sibs => navi_sibs || {},
+		}
 	end
 
 	private
@@ -135,6 +109,37 @@ end
 		size = @sd[:p_size].to_i
 		size = 10 if size < 1
 		item_ids[(page - 1) * size,size].to_a
+	end
+
+	def _item_count(conds)
+		_select_without(:p,conds).size
+	end
+
+	def _sibs(conds)
+		if conds[:id]
+			cid = :id 
+		else 
+			cid = (conds.keys - [:p,:order]).find {|k| respond_to?("_sibs_#{k}",true) }
+		end
+		if cid
+			sibs = __send__("_sibs_#{cid}",conds)
+			{cid => _sort(sibs,conds)} unless sibs.empty?
+		end
+	end
+
+	def _sibs_id(conds)
+		_select_without(:id,conds)
+	end
+
+	def _sibs_d(conds)
+		rex_d = /^\d{#{conds[:d].length}}/
+		_select.collect {|id| id[rex_d] }.uniq
+	end
+
+	def _select_without(cid,conds)
+		c = conds.dup
+		c.delete cid
+		_select(c)
 	end
 
 	def new_id

3bd238e4b92f589f3c07e0af9f9e4453fbe18153
Akira FUNAI <akira@funai.com>
2010-03-02 17:20:24 +0900

    Workflow::Enquete

--- sofa/_workflow.rb
+++ sofa/_workflow.rb
@@ -99,6 +99,25 @@ class Sofa::Workflow::Blog < Sofa::Workflow
 end
 
 
+class Sofa::Workflow::Enquete < Sofa::Workflow
+
+	DEFAULT_META = {
+		:p_size => 10,
+		:conds  => {:p => 'last'},
+	}
+
+	DEFAULT_SUB_ITEMS = {}
+
+	PERM = {
+		:create => 0b0001,
+		:read   => 0b1100,
+		:update => 0b0000,
+		:delete => 0b1100,
+	}
+
+end
+
+
 class Sofa::Workflow::Attachment < Sofa::Workflow
 
 	DEFAULT_META = {

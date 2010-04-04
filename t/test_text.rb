f25f4443d7230e7821afa952284e118ec93f92d2
Akira FUNAI <akira@funai.com>
2010-04-04 15:38:54 +0900

    minor: remove field().

--- t/test_text.rb
+++ t/test_text.rb
@@ -3,80 +3,72 @@
 # Author::    Akira FUNAI
 # Copyright:: Copyright (c) 2009 Akira FUNAI
 
-class TC_Scalar < Test::Unit::TestCase
+class TC_Text < Test::Unit::TestCase
 
 	def setup
+		meta = nil
+		Sofa::Parser.gsub_scalar('$(foo text 3 1..5)') {|id,m|
+			meta = m
+			''
+		}
+		@f = Sofa::Field.instance meta
 	end
 
 	def teardown
 	end
 
-	def test_text_meta
-		f = field '$(foo text 3 1..5)'
+	def test_meta
 		assert_equal(
 			3,
-			f[:size],
+			@f[:size],
 			'Text#initialize should set :size from the token'
 		)
 		assert_equal(
 			1,
-			f[:min],
+			@f[:min],
 			'Text#initialize should set :min from the range token'
 		)
 		assert_equal(
 			5,
-			f[:max],
+			@f[:max],
 			'Text#initialize should set :max from the range token'
 		)
 	end
 
-	def test_text_val_cast
-		f = field '$(foo text 3 1..5)'
+	def test_val_cast
 		assert_equal(
 			'',
-			f.val,
+			@f.val,
 			'Text#val_cast should cast the given val to String'
 		)
 
-		f.load 123
+		@f.load 123
 		assert_equal(
 			'123',
-			f.val,
+			@f.val,
 			'Text#val_cast should cast the given val to String'
 		)
 	end
 
-	def test_text_get
-		f = field '$(foo text 3 1..5)'
-		f.load 'bar'
+	def test_get
+		@f.load 'bar'
 		assert_equal(
 			'bar',
-			f.get,
+			@f.get,
 			'Text#get should return proper string'
 		)
 		assert_equal(
 			'<input type="text" name="" value="bar" class="" />',
-			f.get(:action => :update),
+			@f.get(:action => :update),
 			'Text#get should return proper string'
 		)
 
-		f.load '<bar>'
+		@f.load '<bar>'
 		assert_equal(
 			'&lt;bar&gt;',
-			f.get,
+			@f.get,
 			'Text#get should escape the special characters'
 		)
-	end
-
-	private
-
-	def field(html)
-		meta = nil
-		Sofa::Parser.gsub_scalar(html) {|id,m|
-			meta = m
-			''
-		}
-		Sofa::Field.instance meta
 	end
 
 end

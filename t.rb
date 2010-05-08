c50bef6b7b28c59d84fe2b536c38164b50fbbe08
Akira FUNAI <akira@funai.com>
2010-05-08 22:52:29 +0900

    t.rb call I18n.bindtextdomain().

--- t.rb
+++ t.rb
@@ -11,5 +11,6 @@ require 'mocha'
 require 'sofa'
 
 Sofa.instance_eval { @config = YAML.load_file './t.yaml' }
+Sofa::I18n.bindtextdomain('index','./t/locale')
 
 result = Test::Unit::AutoRunner.run(true,'t/')

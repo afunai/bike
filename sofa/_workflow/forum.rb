55f7041656bf0c0fe95c747988efd724eef4f96f
Akira FUNAI <akira@funai.com>
2010-05-11 14:13:45 +0900

    Workflow::Forum

---
+++ sofa/_workflow/forum.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+
+# Author::    Akira FUNAI
+# Copyright:: Copyright (c) 2009-2010 Akira FUNAI
+
+class Sofa::Workflow::Forum < Sofa::Workflow
+
+	DEFAULT_META = {
+		:p_size     => 10,
+		:item_label => Sofa::I18n.n_('post','posts',1),
+	}
+
+	DEFAULT_SUB_ITEMS = {
+		'_owner'     => {:klass => 'meta-owner'},
+		'_timestamp' => {:klass => 'meta-timestamp'},
+	}
+
+	PERM = {
+		:create => 0b1111,
+		:read   => 0b1111,
+		:update => 0b1100,
+		:delete => 0b1100,
+	}
+
+end

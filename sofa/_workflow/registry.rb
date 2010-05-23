bcbc03ac3ea6e353c0b08a86cbc238250068c19c
Akira FUNAI <akira@funai.com>
2010-05-24 02:41:38 +0900

    Workflow::Registry

---
+++ sofa/_workflow/registry.rb
@@ -0,0 +1,27 @@
+# encoding: UTF-8
+
+# Author::    Akira FUNAI
+# Copyright:: Copyright (c) 2009-2010 Akira FUNAI
+
+class Sofa::Workflow::Registry < Sofa::Workflow
+
+	DEFAULT_META = {
+		:p_size     => 0,
+		:conds      => {:p => '1'},
+		:order      => 'id',
+		:item_label => Sofa::I18n.n_('item','items',1),
+	}
+
+	DEFAULT_SUB_ITEMS = {
+		'_owner'     => {:klass => 'meta-owner'},
+		'_timestamp' => {:klass => 'meta-timestamp'},
+	}
+
+	PERM = {
+		:create => 0b1101,
+		:read   => 0b1110,
+		:update => 0b1110,
+		:delete => 0b1110,
+	}
+
+end

4fb37cb5bfd8a180e744ebdee3d8cc058a525310
Akira FUNAI <akira@funai.com>
2010-05-28 11:23:48 +0900

    move Set::Static::Folder#_g_action_login to _widget/action_login.rb.

---
+++ sofa/_widget/action_login.rb
@@ -0,0 +1,22 @@
+# encoding: UTF-8
+
+# Author::    Akira FUNAI
+# Copyright:: Copyright (c) 2009-2010 Akira FUNAI
+
+class Sofa::Set::Static::Folder
+
+	private
+
+	def _g_action_login(arg)
+		if Sofa.client == 'nobody'
+			<<_html
+<div class="action_login"><a href="#{my[:dir]}/login.html">#{_ 'login...'}</a></div>
+_html
+		else
+			<<_html
+<div class="action_logout"><a href="#{my[:dir]}/logout.html">#{_ 'logout'}</a></div>
+_html
+		end
+	end
+
+end

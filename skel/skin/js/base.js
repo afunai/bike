972b5d043e999cd1333ced94224ba01911b3fca2
Akira FUNAI <akira@funai.com>
2010-06-12 12:04:59 +0900

    hover span.error's.

--- skel/skin/js/base.js
+++ skel/skin/js/base.js
@@ -1,8 +1,40 @@
 $(document).ready(function() {
+  $(':input').hover(
+    function(){
+      $('+ span.error',this).fadeIn(150);
+    },
+    function(){
+      if ($(this).is(':not(.focused)')) $('+ span.error',this).fadeOut(100);
+    }
+  );
+  $(':input').focus(
+    function(){
+      $(this).addClass('focused');
+      $('+ span.error',this).fadeIn(150);
+    }
+  );
+  $(':input').blur(
+    function(){
+      $(this).removeClass('focused');
+      $('+ span.error',this).fadeOut(100);
+    }
+  );
+
+  $('span.error').hide().css({
+    'color':              'white',
+    'background':         'red',
+    'opacity':            '0.5',
+    'position':           'absolute',
+    'margin-left':        '0',
+    'margin-top':         '2px',
+    'padding':            '.3em 1em .3em 1em',
+    '-moz-border-radius': '.3em'
+  });
+
   if ($('.error').length > 0) {
-    $('.error:input:enabled:first').select();
+    $('.error:input:enabled:first').focus().select();
   }
   else {
-    $(':input:enabled:first').select();
+    $(':input:enabled:first').focus().select();
   }
 });
